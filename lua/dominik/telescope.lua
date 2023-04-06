dd9cbe937006539f1a18c84877523ef44caafa8b
Dominik Helfenstein <dominik.helfenstein@bluerange.io>
2023-04-06 09:42:29 +0200

    fix: find_files is now on CTRL-P, git_files <leader>ff

--- lua/dominik/telescope.lua
+++ lua/dominik/telescope.lua
@@ -1,19 +1,19 @@
 local builtin = require('telescope.builtin')
 local actions = require('telescope.actions')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+vim.keymap.set('n', '<leader>ff', builtin.git_files, {})
+vim.keymap.set('n', '<C-p>', builtin.find_files, {})
 vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") });
+    builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
 require "telescope".setup {
-  defaults = {
-    mappings = {
-      i = {
-        ["<C-k>"] = actions.move_selection_previous,
-        ["<C-j>"] = actions.move_selection_next,
-      }
+    defaults = {
+        mappings = {
+            i = {
+                ["<C-k>"] = actions.move_selection_previous,
+                ["<C-j>"] = actions.move_selection_next,
+            }
+        }
     }
-  }
 }
 local telescope = require('telescope')
 telescope.load_extension("fzf")
