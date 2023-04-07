e98ee5c7f8f5b09933609e4534090a9a16dff00e
Dominik Helfenstein <dominik.helfenstein@gmx.de>
2023-04-07 23:38:45 +0200

    feat: live grep

--- lua/dominik/telescope.lua
+++ lua/dominik/telescope.lua
@@ -2,9 +2,7 @@ local builtin = require('telescope.builtin')
 local actions = require('telescope.actions')
 vim.keymap.set('n', '<leader>ff', builtin.git_files, {})
 vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-vim.keymap.set('n', '<leader>ps', function()
-    builtin.grep_string({ search = vim.fn.input("Grep > ") });
-end)
+vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
 require "telescope".setup {
     defaults = {
         mappings = {
